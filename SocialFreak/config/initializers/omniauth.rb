fe8d12cf495214471506c0040bcae229ceecb84f
thunderenlight <thunderenlight@gmail.com>
2016-04-13 15:21:07 -0500

    Adding google outh

--- SocialFreak/config/initializers/omniauth.rb
+++ SocialFreak/config/initializers/omniauth.rb
@@ -1,13 +1,23 @@
+	
+tkey = 'VVJTUONirmCDwkDePgpgLoK60'
+tsec = 'r2sd6DDRsMmK3RXsAuEEqF7CGx7Yqk05wpsS7WBELXFzJ4kyaY'
+gkey = '92961498578-h7uoof6r38o0gjh67fhv4hvupd5t9ifg.apps.googleusercontent.com'
+gsec = '_0g7y4uKCIpSPNYs6H5amUog'
 Rails.application.config.middleware.use OmniAuth::Builder do 
-	provider :twitter, ENV['VVJTUONirmCDwkDePgpgLoK60'], ENV['r2sd6DDRsMmK3RXsAuEEqF7CGx7Yqk05wpsS7WBELXFzJ4kyaY'],
-	  {
-      :secure_image_url => 'true',
-      :image_size => 'original',
-      :authorize_params => {
-        :force_login => 'true',
-        :lang => 'pt'
-      }
-    }
+ provider :twitter, ENV[tkey], ENV[tsec]
+
+
+ provider :google_oauth2, client_id:'92961498578-h7uoof6r38o0gjh67fhv4hvupd5t9ifg.apps.googleusercontent.com' , client_secret:'_0g7y4uKCIpSPNYs6H5amUog',
+           scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google',
+           skip_jwt: true
+
+
+  OmniAuth.config.on_failure = Proc.new do |env|
+    SessionsController.action(:auth_failure).call(env)
+    # error_type = env['omniauth.error.type']
+    # new_path = "#{env['SCRIPT_NAME']}#{OmniAuth.config.path_prefix}/failure?message=#{error_type}"
+    # [301, {'Location' => new_path, 'Content-Type' => 'text/html'}, []]
+  end
 end
 
 
@@ -15,6 +25,3 @@ end
 
 #  Consumer Key (API Key) VVJTUONirmCDwkDePgpgLoK60
 # Consumer Secret (API Secret) r2sd6DDRsMmK3RXsAuEEqF7CGx7Yqk05wpsS7WBELXFzJ4kyaY 
-	
-# "TWITTER_KEY" = 'VVJTUONirmCDwkDePgpgLoK60'
-# 'TWITTER_SECRET' = 'r2sd6DDRsMmK3RXsAuEEqF7CGx7Yqk05wpsS7WBELXFzJ4kyaY'
--- SocialFreak/config/routes.rb
+++ SocialFreak/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-root to: 'pages#home'
+  root to: 'pages#index'
 
-get '/auth/:provider/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/failure', to: 'sessions#auth_failure'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
